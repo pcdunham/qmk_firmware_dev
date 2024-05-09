d4a900290e0f2a76856d031af51bd7956b9c28ef
pcdunham <120682897+pcdunham@users.noreply.github.com>
2024-05-08 21:19:35 -0500

    Q0 keymap

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/pcd/macros.h
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/pcd/macros.h
@@ -1,28 +1,14 @@
 
 enum custom_keycodes {
-    EMAIL = SAFE_RANGE,
-    WINTERM,
-    CLRMODS
+    EMAIL = SAFE_RANGE
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case WINTERM:
-            if (record->event.pressed) {
-                tap_code16(LGUI(KC_R));
-                wait_ms(100);
-                SEND_STRING("wt");
-                wait_ms(50);
-                tap_code16(KC_ENT);
-            }
+
         case EMAIL:
             if (record->event.pressed) {
                 SEND_STRING("p.c.dunham@gmail.com");
-            }
-        case CLRMODS:
-            if (record->event.pressed) {
-                cancel_key_lock();
-                clear_mods();
             }
             return false;
     }
