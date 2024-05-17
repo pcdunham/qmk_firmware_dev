39f7c5d768cd404809b4ee2ec8b4fd5f2f2d03e3
pcdunham <120682897+pcdunham@users.noreply.github.com>
2024-05-17 07:40:51 -0500

    renamed gmmk2 layers

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/pcd/keymap.c
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/pcd/keymap.c
@@ -62,10 +62,9 @@ int SIZE_OF_TO_KEYS = sizeof(TO_KEYS)/sizeof(int);
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum custom_layers {
-    _BL,
-    _FL,
-    _1L,
-    _2L
+    LBase,
+    LApp,
+    LFunc
 };
 
 
@@ -81,41 +80,32 @@ enum custom_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    // Keymap _BL: Base Layer (Default Layer)
+    // Keymap LBase: Base Layer (Default Layer)
 
-[_BL] = LAYOUT(
+[LBase] = LAYOUT(
   KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   QK_LOCK,  KC_PSCR,  KC_DEL,   KC_INS,   KC_PGUP,  KC_PGDN,
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_P4,    KC_P5,    KC_P6,
   OSM(MOD_LSFT),KC_Z, KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
-  OSM(MOD_LCTL),OSM(MOD_LGUI),OSM(MOD_LALT),        KC_SPC,                                 KC_RALT,  OSL(_FL), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
+  OSM(MOD_LCTL),OSM(MOD_LGUI),OSM(MOD_LALT),        KC_SPC,                                 KC_RALT,  OSL(LFunc), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
 
     // Custom Layer for specific applications
 
-[_1L] = LAYOUT(
-  TO(_BL),  KC_MYCM,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+[LApp] = LAYOUT(
+  TO(LBase),  KC_MYCM,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_TRNS,  MO(_FL),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_TRNS,  MO(LFunc),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
-    // Blender
-
-[_2L] = LAYOUT(
-  TO(_BL),  KC_MYCM,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  QK_LOCK,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  QK_GESC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_RSFT,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_RALT,  MO(_FL),  KC_RCTL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
     // Function layer
 
-[_FL] = LAYOUT(
+[LFunc] = LAYOUT(
   QK_BOOT,  KC_MYCM,  KC_WHOM,  _______,  KC_CPNL,  KC_WBAK,  KC_WFWD,  KC_MPLY,  KC_MSTP,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,   _______,  _______,  _______,  _______,  _______,
-  _______,  TO(_1L),  TO(_2L),  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(_BL),  _______,  _______,   _______,  _______,  _______,  _______,  _______,
+  _______,  TO(LApp), _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(LBase),_______,  _______,   _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  EMAIL,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             _______,  _______,  _______,  _______,
   _______,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_SPI,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,             RGB_VAI,  _______,  _______,  _______,  _______,
@@ -195,7 +185,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     // Set specified keys to colors depending on which layer is selected
     switch(get_highest_layer(layer_state)) {
-        case _BL:  //Layer 0
+        case LBase:  //Layer 0
             set_glorious_side_96('r', ORANGE_RGB);
             set_glorious_side_96('l', ORANGE_RGB);
             // First argument of the two functions below correspond to LED/Key numbers from LED layout above. Remaining arguments are color values.
@@ -204,11 +194,11 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             rgb_matrix_set_color(92, RGB_MAGENTA);
 
             break;
-        case _1L:  //layer 1
+        case LApp:  //layer 1
             // Set number key row LED to identify which layer is active
             rgb_matrix_set_color(19, 255, 255, 255);
-            set_glorious_side_96('r', RGB_RED);
-            set_glorious_side_96('l', RGB_RED);
+            set_glorious_side_96('r', RGB_AZURE);
+            set_glorious_side_96('l', RGB_AZURE);
             // Arrow Keys
             for(int i = 0; i < SIZE_OF_ARROW_KEYS; i++) {
                 rgb_matrix_set_color(ARROW_KEYS[i], ARROW_KEY_COLORS);
@@ -230,13 +220,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
             break;
 
-        case _2L:  //layer 2
-            rgb_matrix_set_color(20, 255, 255, 255);
-            set_glorious_side_96('r', RGB_AZURE);
-            set_glorious_side_96('l', RGB_AZURE);
-            break;
-
-        case _FL:  //layer 3
+        case LFunc:  //layer 3
             // Set number key 3 color to identify it asa the active layer
             rgb_matrix_set_color(21, 255, 255, 255);
             set_glorious_side_96('r', RGB_MAGENTA);
