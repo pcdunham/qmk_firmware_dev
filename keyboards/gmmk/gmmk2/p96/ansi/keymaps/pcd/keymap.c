c556a86a30282029e35a22bd071448be230413b4
pcdunham <120682897+pcdunham@users.noreply.github.com>
2024-05-19 11:09:56 -0500

    minor cleanup

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/pcd/keymap.c
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/pcd/keymap.c
@@ -63,7 +63,7 @@ int SIZE_OF_TO_KEYS = sizeof(TO_KEYS)/sizeof(int);
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum custom_layers {
     LBase,
-    LApp,
+    // LApp,
     LFunc
 };
 
@@ -83,14 +83,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Keymap LBase: Base Layer (Default Layer)
 
 [LBase] = LAYOUT(
-  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   QK_LOCK,  KC_PSCR,  KC_DEL,   KC_INS,   KC_PGUP,  KC_PGDN,
+  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   QK_LOCK,  KC_PSCR,  KC_DEL,   MO(LFunc),   KC_PGUP,  KC_PGDN,
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_P4,    KC_P5,    KC_P6,
   OSM(MOD_LSFT),KC_Z, KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
-  OSM(MOD_LCTL),OSM(MOD_LGUI),OSM(MOD_LALT),        KC_SPC,                                 KC_RALT,  OSL(LFunc), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
+  OSM(MOD_LCTL),OSM(MOD_LGUI),OSM(MOD_LALT),        KC_SPC,                                 KC_RALT,  OSL(LFunc),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
 
-    // Custom Layer for specific applications
+/*     // Custom Layer for specific applications
 
 [LApp] = LAYOUT(
   TO(LBase),  KC_MYCM,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -99,13 +99,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_TRNS,  MO(LFunc),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
-
+ */
 
     // Function layer
 
 [LFunc] = LAYOUT(
   QK_BOOT,  KC_MYCM,  KC_WHOM,  _______,  KC_CPNL,  KC_WBAK,  KC_WFWD,  KC_MPLY,  KC_MSTP,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,   _______,  _______,  _______,  _______,  _______,
-  _______,  TO(LApp), _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(LBase),_______,  _______,   _______,  _______,  _______,  _______,  _______,
+  _______,  TO(LBase),_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(LBase),_______,  _______,   _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  EMAIL,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             _______,  _______,  _______,  _______,
   _______,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_SPI,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,             RGB_VAI,  _______,  _______,  _______,  _______,
@@ -189,14 +189,15 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             set_glorious_side_96('r', ORANGE_RGB);
             set_glorious_side_96('l', ORANGE_RGB);
             // First argument of the two functions below correspond to LED/Key numbers from LED layout above. Remaining arguments are color values.
-            rgb_matrix_set_color(28, 255, 255, 255);
+            rgb_matrix_set_color(19, 255, 255, 255);
             // Set FN Key Color
             rgb_matrix_set_color(92, RGB_MAGENTA);
+            rgb_matrix_set_color(93, RGB_MAGENTA);
 
             break;
-        case LApp:  //layer 1
+        /* case LApp:  //layer 1
             // Set number key row LED to identify which layer is active
-            rgb_matrix_set_color(19, 255, 255, 255);
+            rgb_matrix_set_color(20, 255, 255, 255);
             set_glorious_side_96('r', RGB_AZURE);
             set_glorious_side_96('l', RGB_AZURE);
             // Arrow Keys
@@ -218,13 +219,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             // ESC Key
             rgb_matrix_set_color(ESC_KEY, ESC_KEY_COLORS);
 
-            break;
+            break; */
 
-        case LFunc:  //layer 3
+        case LFunc:  //layer 1
             // Set number key 3 color to identify it asa the active layer
-            rgb_matrix_set_color(21, 255, 255, 255);
-            set_glorious_side_96('r', RGB_MAGENTA);
-            set_glorious_side_96('l', RGB_MAGENTA);
+            rgb_matrix_set_color(20, 255, 255, 255);
+            set_glorious_side_96('r', RGB_AZURE);
+            set_glorious_side_96('l', RGB_AZURE);
             // KC_MYCM,  KC_WHOM,  KC_CALC,  KC_MSEL, UC_M_WI
             for(int i = 0; i < SIZE_OF_MISC_KEYS; i++) {
                 rgb_matrix_set_color(MISC_KEYS[i], MISC_KEY_COLORS);
